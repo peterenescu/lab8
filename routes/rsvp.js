d63115018f117be001cfc3b4e05b20a3fc652a51
Peter Enescu <peter.enescu@gmail.com>
2019-02-28 09:55:36 -0800

    Completed Lab 8

--- routes/rsvp.js
+++ routes/rsvp.js
@@ -5,6 +5,18 @@ var data = {
 /*
  * GET tasks page.
  */
+exports.addRSVP = function(request, response){
+var rsvpEmail = request.body.rsvpEmail;
+  // This will print to your terminal whenever the POST request is made
+  console.log(rsvpEmail);
+
+  data.rsvp.push(rsvpEmail); // Add to current data
+
+  // Let us send back some data so that
+  // browser knows it worked!
+  response.send(rsvpEmail);
+}
+
 exports.adminView = function(request, response){
   response.render('rsvp', data);
 };
